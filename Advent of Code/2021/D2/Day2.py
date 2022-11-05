f2b5034d2d99bb36a4c24d4db8d3b5fb14582ebf
Prasuk Jain <pj9889@srmist.edu.in>
2022-11-06 02:38:17 +0530

    Commit

---
+++ Advent of Code/2021/D2/Day2.py
@@ -0,0 +1,18 @@
+#2021
+#Day2 Problem1
+
+with open("AOC D2.txt", "r") as inputs:
+    lines = inputs.readlines()
+    numbers = [entry.strip() for entry in lines]
+
+x = 0
+y = 0
+for i in numbers:
+    direction, amount = numbers[0].split(' '), int(numbers[0].split(' '))
+    if 'forward' in direction:
+        x += amount
+    elif 'down' in direction:
+        y -= amount
+    elif 'up' in direction:
+        y += amount
+print(x*y)
