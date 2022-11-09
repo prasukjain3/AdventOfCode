12225bff4d1f663263ddf6d5ebbc645413cada4d
Prasuk Jain <pj9889@srmist.edu.in>
2022-11-09 10:30:50 +0530

    Commit

--- Advent of Code/2021/D3/Day3.py
+++ Advent of Code/2021/D3/Day3.py
@@ -3,12 +3,12 @@
 
 with open("AOT D3.txt", "r") as inputs:
     lines = inputs.readlines()
-    numbers = [entry.strip() for entry in lines]
+    diagnostic = [entry.strip() for entry in lines]
 
 gamma, epsilon = '', ''
-for i in range(len(numbers[0])):
-    all_entries_at_pos = [entry[i] for entry in numbers]
-    if all_entries_at_pos.count('0') > len(numbers)/2:
+for i in range(len(diagnostic[0])):
+    all_entries_at_pos = [entry[i] for entry in diagnostic]
+    if all_entries_at_pos.count('0') > len(diagnostic)/2:
         gamma += '1'
         epsilon += '0'
     else:
@@ -16,3 +16,32 @@ for i in range(len(numbers[0])):
         epsilon += '1'
 power_factor = int(gamma, base = 2)*int(epsilon, base = 2)
 print(power_factor)
+
+from copy import copy
+
+oxy_diagnostics = copy(diagnostic)
+for i in range(len(diagnostic)):
+    if len(oxy_diagnostics) == 1:
+        break
+    all_entries_at_pos = [entry[i] for entry in oxy_diagnostics]
+    common_bit = '1' if all_entries_at_pos.count("1") >= len(oxy_diagnostics)/2\
+        else '0'
+    oxy_diagnostics = [entry for entry in oxy_diagnostics
+                       if entry[i] ==common_bit]
+oxy_rating = int(oxy_diagnostics[0], base=2)
+print("Oxygen ", oxy_diagnostics[0], oxy_rating)
+
+
+co2_diagnostics = copy(diagnostic)
+for i in range(len(diagnostic)):
+    if len(co2_diagnostics) == 1:
+        break
+    all_entries_at_pos = [entry[i] for entry in co2_diagnostics]
+    common_bit = '0' if all_entries_at_pos.count("1") >= len(co2_diagnostics)/2\
+        else '1'
+    co2_diagnostics = [entry for entry in co2_diagnostics
+                       if entry[i] ==common_bit]
+co2_rating = int(co2_diagnostics[0], base=2)
+print("Oxygen ", co2_diagnostics[0], co2_rating)
+
+print("Life Rating: ", oxy_rating*co2_rating)
